82ee0e59fff3f1ab999c50a50e3b90af89f31b81
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-10 18:20:35 +0100

    ottimizzato il layout del componente card aggiungendo classi Bootstrap e migliorando la visualizzazione dell'immagine e del titolo

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,6 @@
-<div class="card border border-secondary-subtle rounded-0 card-img-top">
-    <img src="{{ $immagine }}" alt="immagini fumetti">
-    <h4>{{ $titolo }}</h4>
+<div class="card border border-secondary-subtle rounded-0 my-5">
+    <img src="{{ $immagine }}" alt="immagini fumetti" class="img-fluid card-img-top"
+        style="width: 200px; height: 300px; object-fit: cover;">
+    <p class="text-uppercase fw-bold" style="font-size:12px">{{ $titolo }}</p>
     {{ $slot }}
 </div>
