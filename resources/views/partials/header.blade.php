373ec61545d7827ddcc9176d17788588ad7bc9df
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-10 17:01:44 +0100

    aggiunto array associativo per elenco di link nel componente header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,5 +2,24 @@
     <nav>
         logo DC e link pagina
         <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC header">
+        @php
+            $headerLinks = [
+                'characters' => 'Characters',
+                'comics' => 'Comics',
+                'movies' => 'Movies',
+                'tv' => 'TV',
+                'games' => 'Games',
+                'collectibles' => 'Collectibles',
+                'videos' => 'Videos',
+                'fans' => 'Fans',
+                'news' => 'News',
+                'shop' => 'Shop',
+            ];
+        @endphp
+        <ul>
+            @foreach ($headerLinks as $key => $label)
+                <li class="text-uppercase">{{ $label }}</li>
+            @endforeach
+        </ul>
     </nav>
 </header>
