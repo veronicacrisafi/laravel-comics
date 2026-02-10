0042d7875b9883f2cac4992a8fc08341df9bd7d3
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-10 12:01:12 +0100

    riordinato il codice in home.blade.php spostando il blocco PHP all'inizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.master')
+@php
+    $cards = config('comics');
+    dd($cards);
+@endphp
 @section('contenuto')
     <h1>DC COMICS</h1>
 
     <x-card> contenuto card</x-card>
 @endsection
-
-@php
-    $cards = config('comics');
-    dd($cards);
-@endphp
