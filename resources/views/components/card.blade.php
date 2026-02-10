a3bd9a07a518bce3adefa21357c777784853eefb
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-10 18:39:54 +0100

    ripristinato il layout del componente card rimuovendo il bordo e ottimizzando la visualizzazione dell'immagine

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
-<div class="card border border-secondary-subtle rounded-0 my-5">
-    <img src="{{ $immagine }}" alt="immagini fumetti" class="img-fluid card-img-top"
-        style="width: 200px; height: 300px; object-fit: cover;">
-    <p class="text-uppercase fw-bold" style="font-size:12px">{{ $titolo }}</p>
+<div class="card border-0 rounded-0 my-5">
+    <img src="{{ $immagine }}" alt="immagini fumetti" class="img-fluid"
+        style="width: 300px; height: 300px; object-fit: cover;">
+    <p class="text-uppercase fw-bold pt-3" style="font-size:12px">{{ $titolo }}</p>
     {{ $slot }}
 </div>
