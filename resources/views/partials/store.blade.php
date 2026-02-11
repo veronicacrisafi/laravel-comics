4ed4ec17a6cf43e3b232de3489c897028dbf429e
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-11 16:43:16 +0100

    ottimizzato il layout della sezione store aggiungendo margini ai link per migliorare l'allineamento e la leggibilità

--- resources/views/partials/store.blade.php
+++ resources/views/partials/store.blade.php
@@ -1,31 +1,31 @@
 <div class="bg-primary">
     <div class="container">
         <div class="row mt-5 pt-3">
-            <ul class="d-flex justify-content-around list-unstyled">
+            <ul class="d-flex justify-content-around list-unstyled align-items-center">
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="digital comics"
                         style="width:50px">
-                    <a href="" class="text-light text-decoration-none">DIGITAL COMICS</a>
+                    <a href="" class="text-light text-decoration-none ms-2">DIGITAL COMICS</a>
                 </li>
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="dc merchandise"
                         style="width:60px">
-                    <a href="" class="text-light text-decoration-none">DC MERCHANDISE</a>
+                    <a href="" class="text-light text-decoration-none ms-2">DC MERCHANDISE</a>
                 </li>
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="subscription"
                         style="width:60px">
-                    <a href="" class="text-light text-decoration-none">SUBSCRIPTION</a>
+                    <a href="" class="text-light text-decoration-none ms-2">SUBSCRIPTION</a>
                 </li>
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="comic shop locator"
                         style="width:40px">
-                    <a href="" class="text-light text-decoration-none">COMIC SHOP LOCATOR</a>
+                    <a href="" class="text-light text-decoration-none ms-2">COMIC SHOP LOCATOR</a>
                 </li>
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="dc power visa"
                         style="width:70px">
-                    <a href="" class="text-light text-decoration-none">DC POWER VISA</a>
+                    <a href="" class="text-light text-decoration-none ms-2 ">DC POWER VISA</a>
                 </li>
             </ul>
         </div>
