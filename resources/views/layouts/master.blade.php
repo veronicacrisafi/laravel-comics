28837990efc2fedbc5ee92887e7799c454e4ad4d
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-11 17:49:36 +0100

    aggiunto il foglio di stile app.css e aggiornati i link nel partial store per migliorare l'aspetto e la funzionalità

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ Vite::asset('resources/css/app.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Laravel-comics</title>
 </head>
--- resources/views/partials/store.blade.php
+++ resources/views/partials/store.blade.php
@@ -5,27 +5,27 @@
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="digital comics"
                         style="width:50px">
-                    <a href="" class="text-light text-decoration-none ms-2">DIGITAL COMICS</a>
+                    <a href="#" class="text-light text-decoration-none ms-2">DIGITAL COMICS</a>
                 </li>
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="dc merchandise"
                         style="width:60px">
-                    <a href="" class="text-light text-decoration-none ms-2">DC MERCHANDISE</a>
+                    <a href="#" class="text-light text-decoration-none ms-2">DC MERCHANDISE</a>
                 </li>
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="subscription"
                         style="width:60px">
-                    <a href="" class="text-light text-decoration-none ms-2">SUBSCRIPTION</a>
+                    <a href="#" class="text-light text-decoration-none ms-2">SUBSCRIPTION</a>
                 </li>
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="comic shop locator"
                         style="width:40px">
-                    <a href="" class="text-light text-decoration-none ms-2">COMIC SHOP LOCATOR</a>
+                    <a href="#" class="text-light text-decoration-none ms-2">COMIC SHOP LOCATOR</a>
                 </li>
                 <li>
                     <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="dc power visa"
                         style="width:70px">
-                    <a href="" class="text-light text-decoration-none ms-2 ">DC POWER VISA</a>
+                    <a href="#" class="text-light text-decoration-none ms-2 ">DC POWER VISA</a>
                 </li>
             </ul>
         </div>
