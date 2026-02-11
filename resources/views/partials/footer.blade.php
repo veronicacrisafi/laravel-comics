15771cdaefee35d3397228aa387455ebcf264c36
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-11 16:29:29 +0100

    aggiunto il footer con struttura e stili per migliorare l'aspetto e la funzionalità

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,39 @@
-<footer class="">
-
+<footer class="bg-dark">
+    <div class="container py-4">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-auto">
+                <a href="#" class="btn btn-dark border border-primary rounded-0 text-light px-4">SIGN-UP NOW!</a>
+            </div>
+            <div class="col-auto d-flex align-items-center">
+                <h6 class="text-primary mb-0 me-3">FOLLOW US</h6>
+                <ul class="d-flex list-unstyled mb-0">
+                    <li class="ms-2">
+                        <a href="#" class="text-decoration-none">
+                            <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook">
+                        </a>
+                    </li>
+                    <li class="ms-2">
+                        <a href="#" class="text-decoration-none">
+                            <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter">
+                        </a>
+                    </li>
+                    <li class="ms-2">
+                        <a href="#" class="text-decoration-none">
+                            <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YouTube">
+                        </a>
+                    </li>
+                    <li class="ms-2">
+                        <a href="#" class="text-decoration-none">
+                            <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest">
+                        </a>
+                    </li>
+                    <li class="ms-2">
+                        <a href="#" class="text-decoration-none">
+                            <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
