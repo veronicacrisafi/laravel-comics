47b74bc3e103f0706fbeafe0e3b5ad1548ac08aa
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-12 12:15:18 +0100

    modificato il titolo della card per utilizzare il testo maiuscolo e migliorare la presentazione

--- resources/views/singleCard.blade.php
+++ resources/views/singleCard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('contenutoCard')
     <div class="py-5 text-center">
-        <h1>prova altre pagine</h1>
+        <h1 class="text-uppercase">prova altre pagine</h1>
         <a href="{{ route('home') }}" class="btn btn-primary rounded-0">Ritorna alla Home</a>
     </div>
 @endsection
